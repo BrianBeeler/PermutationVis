902f12682ff2f600811f3850920b89196659ae02
Brian Beeler <brianlbeeler@gmail.com>
2015-08-30 20:22:53 -0500

    updated readme, added css

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PermutationVis
 
-Goal : Establish a decision tree visualization for permutations of any input
+Goal   : Establish a decision tree visualization for permutations of any input
+Status : Backlogged, will pick up after other projects end. 
 
 Author: Brian Beeler
--- app.js
+++ app.js
@@ -1,44 +1,45 @@
+// Experimenting with D3 trees before creating alogirithms
+
 d3.select('body').style("background-color", "blue")
 
+var treeData = [
+  {
+    "name": "Top Level",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Top Level",
+        "children": [
+          {
+            "name": "Son of A",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Daughter of A",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "parent": "Top Level"
+      }
+    ]
+  }
+];
 
-var treeData2 = [];
-
-var insert = function(level) {
-
-}
-
-
-
-var treeData = [];
-
-var Tree = function(root) {
-  
-  var tree  = {};
-  var count = 
-
-  tree.data = [];
- 
-
-  tree.add = function(parent) {}
-
-
-
-
-
-} 
-
-// ************** Generate the tree diagram  *****************
-var margin = {top: 20, right: 140, bottom: 20, left: 140},
- width = 960 - margin.right - margin.left,
- height = 500 - margin.top - margin.bottom;
+var margin = {top: 20, right: 120, bottom: 20, left: 120}
+var width = 960 - margin.right - margin.left
+var height = 500 - margin.top - margin.bottom
  
 var i = 0;
 
 var tree = d3.layout.tree()
- .size([height, width]);
+ .size([height, width]); 
 
 var diagonal = d3.svg.diagonal()
- .projection(function(d) { return [d.y, d.x]; });
+.projection(function(d) { return [d.y, d.x]; });
 
 var svg = d3.select("body").append("svg")
  .attr("width", width + margin.right + margin.left)
@@ -47,49 +48,23 @@ var svg = d3.select("body").append("svg")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 root = treeData[0];
-  
 update(root);
 
-function update(source) {
+function update(root) {
+  var nodes = tree.nodes(root),
+  var links = tree.links(nodes);
+  nodes.forEach(function(d) { d.y = d.depth * 180; })
+}
 
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-   links    = tree.links(nodes);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-  // Declare the nodesâ€¦
-  var node = svg.selectAll("g.node")
+ var node = svg.selectAll("g.node")
    .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-  // Enter the nodes.
   var nodeEnter = node.enter().append("g")
    .attr("class", "node")
    .attr("transform", function(d) { 
     return "translate(" + d.y + "," + d.x + ")"; });
 
-  nodeEnter.append("circle")
-   .attr("r", 10)
-   .style("fill", "#fff");
 
-  nodeEnter.append("text")
-   .attr("x", function(d) { 
-    return d.children || d._children ? -13 : 13; })
-   .attr("dy", ".35em")
-   .attr("text-anchor", function(d) { 
-    return d.children || d._children ? "end" : "start"; })
-   .text(function(d) { return d.name; })
-   .style("fill-opacity", 1);
 
-  // Declare the link¦
-  var link = svg.selectAll("path.link")
-   .data(links, function(d) { return d.target.id; });
 
-  // Enter the links.
-  link.enter().insert("path", "g")
-   .attr("class", "link")
-   .attr("d", diagonal);
 
-}
 
